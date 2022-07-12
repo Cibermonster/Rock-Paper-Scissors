7ca4d715a951d910098d4538547056055a18dcbf
Ciber <cibermonster@gmail.com>
2022-07-12 11:45:02 -0400

    Add validPlays Array

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function playRound(playerSelection, computerSelection) {
-
+    //let pick = prompt("Your selection:")
 }
 
 function computerPlay() {
+    
 }
+
+const validPlays = ["Rock", "Paper", "Scissors"]
 
 const playerSelection = "rock";
 const computerSelection = computerPlay();
