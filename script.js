e22ac4377b17b8b48c479bed04d5912ed9fc49fb
Ciber <cibermonster@gmail.com>
2022-07-12 12:06:08 -0400

    Add PlayerPick + Validate

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 function playRound(playerSelection, computerSelection) {
-    //let pick = prompt("Your selection:")
+    //play round stuff
 }
 
 function computerPlay() {
     let randNum = Math.floor(Math.random()*3)
     return validPlays[randNum]
 }
+function playerPick() {
+    var isvalidpick = false
+    while (isvalidpick != true) {
+        let pick = prompt("Your selection:")
+        var isvalidpick = validatePlay(pick)
+    }
+}
+function validatePlay(play) {
+    if (validPlays.indexOf(play) >= 0) {
+        return true
+    }
+    return false
+}
 
 const validPlays = ["Rock", "Paper", "Scissors"]
 
-const playerSelection = "rock";
+const playerSelection = playerPick();
 const computerSelection = computerPlay();
 //console.log(playRound(playerSelection, computerSelection));
 console.log(computerPlay())
