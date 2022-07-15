567a28fcc8c70c436d25568849f484949277515e
Ciber <cibermonster@gmail.com>
2022-07-15 10:10:42 -0400

    Cleanup

--- script.js
+++ script.js
@@ -14,10 +14,8 @@ buttons.forEach((button) => {
 });
 
 function playRound(playerSelection, computerSelection) {
-
     document.getElementById("playerHand").innerHTML = '<img src="./img/'+playerSelection+'.png" />';
     document.getElementById("computerHand").innerHTML = '<img src="./img/'+computerSelection+'.png" />';
-
     if (playerSelection == computerSelection) {
         return "T"
     } else if (playerSelection == "rock" && computerSelection == "paper") {
@@ -36,22 +34,6 @@ function computerPlay() {
     let randNum = Math.floor(Math.random()*3)
     return validPlays[randNum]
 }
-
-/*function playerPick() {
-    var isvalidpick = false
-    while (isvalidpick != true) {
-        var pick = prompt("Your selection:").toLocaleLowerCase()
-        var isvalidpick = validatePlay(pick)
-    }
-    return pick
-}
-
-function validatePlay(play) {
-    if (validPlays.indexOf(play) >= 0) { // Check if player's pick is valid
-        return true
-    }
-    return false
-}*/
 
 function game(id) {
     let result = playRound(id, computerPlay());
@@ -74,13 +56,6 @@ function game(id) {
 
     document.getElementById("playerscore").innerHTML = "Player: " + scoreWin;
     document.getElementById("computerscore").innerHTML = "Computer: " + scoreLoss;
-/*const container = document.querySelector('#results');
-
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = "Wins: " + scoreWin + " Lost: " + scoreLoss + " Tied: " + scoreTie + " Rounds: " + round;
-
-container.appendChild(content);*/
 }
 
 
